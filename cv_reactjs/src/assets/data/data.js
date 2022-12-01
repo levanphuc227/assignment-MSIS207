e1823afbbc04473313763afe7add29bf63396aae
DESKTOP-KBPUDJV\admin <phuc112112112@gmail.com>
2022-12-01 20:22:48 +0700

    first commit

---
+++ cv_reactjs/src/assets/data/data.js
@@ -0,0 +1,71 @@
+data={
+    "inf": {
+       "name":"Le Van Phuc",
+        "NgaySinh":"27/02/2001",
+        "GioiTinh":"Nam",
+        "SoDienThoai":"0123456789",
+        "email":"admin@gmail.com",
+        "address":"quận thủ đức, hồ chí minh"
+     },
+     
+     "array": [
+       {
+         "heading":"education",
+         "listcontent":[
+           {
+             "timestart":"2019",
+             "timeend":"now",
+             "heading":"UIT",
+             "content":"major: Information system\n gpa:2/4"
+             
+           }
+           
+         ]
+       },
+       {
+         "heading":"skill",
+         "listcontent":[
+           {
+             "timestart":null,
+             "timeend":null,
+             "heading":"Nodejs",
+             "content":"Đã có 2 dự án liên quan đến nodejs. trong dự án có sử dụng framework express có phân quyền cho group người dùng"
+             
+           },
+           {
+             "timestart":null,
+             "timeend":null,
+             "heading":"javascript",
+             "content":"Hiểu và nắm được một số phần quan trọng như promise, callback, bất đồng bộ"
+             
+           },{
+             "timestart":null,
+             "timeend":null,
+             "heading":"html,css",
+             "content":"Có khả năng làm web responsive"
+             
+           }
+           
+         ]
+       },
+       {
+         "heading":"project",
+         "listcontent":[
+           {
+             "timestart":null,
+             "timeend":null,
+             "heading":"Web bán điện thoại",
+             "content":"ở dự án này đã sữ dụng có các ngôn ngữ nodejs, reactjs.dự án này có những tính năng tiêu biểu như crud danh mục phẩm, sản phẩm, group users. tính năng nỗi bật mỗi sản phẩm có nhìu opition cho người dùng chọn mua sản mua. những dữ liệu được lưu trữ trên mysql dữ liệu khi thực hiện các thao tác crud đảm bảo được tính toàn vẹn giữa liệu"
+           },
+           {
+             "timestart":null,
+             "timeend":null,
+             "heading":"Web bán sách",
+             "content":"ở dự án này đã sữ dụng có các ngôn ngữ nodejs, reactjs.dự án này có những tính năng tiêu biểu như crud danh mục phẩm, sản phẩm, group users. trang web có phần danh mục phẩm đa cấp. những dữ liệu được lưu trữ trên mysql dữ liệu khi thực hiện các thao tác crud đảm bảo được tính toàn vẹn giữa liệu"
+             
+           }
+           
+         ]
+       }
+     ]
+   }
